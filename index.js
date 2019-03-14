94d30c07a3b352ce47bde7db8d8ad2080a184381
Quentin Le Rolland <quentin.lerolland@epitech.eu>
2019-03-14 12:43:15 +0100

    Socket

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const net = require('net');
 
-const client = new net.Socket();
+const rl = require('readline');
 
-client.connect(15555, "172.20.10.2", (error, res) => {
-  console.log(error, res)
+const client1 = new net.Socket();
+const client2 = new net.Socket();
+
+client1.connect(15555, "10.38.165.60", () => {
+  console.log('Client 1 connected to server')
+});
+
+client2.connect(15555, "10.38.165.60", () => {
+  console.log('Client 2 connected to server')
+});
+
+
+client1.on('data', (data) => {
+  try {
+    const decode = data.toString('ascii');
+    console.log(decode.replace('@', '').split(/[^ -~]+/g).map(JSON.parse));
+      // console.log('Client1 received', JSON.parse(str.toString('ascii')));
+  } catch {
+    console.log('Client1 Error while parsing data')
+  }
+});
+
+
+client2.on('data', (data) => {
+  try {
+    const decode = data.toString('ascii');
+    console.log(decode.split('\r'));
+  } catch {
+    console.log('Client2 Error while parsing data')
+  }
 });
